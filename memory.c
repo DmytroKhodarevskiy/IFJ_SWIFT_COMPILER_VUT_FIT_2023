5ae03e6b7cad02471704f16dbf740c32949bfc49
chivapchicha <xhryny00@vutbr.cz>
2023-11-22 16:22:25 +0100

    memory&errors

--- memory.c
+++ memory.c
@@ -7,80 +7,68 @@
 
 #include "memory.h"
 
-void initializeDynamicArray() {
-    allocatedMemoryArray.capacity = 10; 
-    allocatedMemoryArray.size = 0;
-    allocatedMemoryArray.data = (void**) malloc(allocatedMemoryArray.capacity * sizeof(void*));
-    if (allocatedMemoryArray.data == NULL) {
-        exitWithError("Failed to initialize memory array", MST_INTERNAL);
-    }
-}
 
-void resizeDynamicArray() {
-    allocatedMemoryArray.capacity *= 2;
-    allocatedMemoryArray.data = (void**) realloc(allocatedMemoryArray.data, allocatedMemoryArray.capacity * sizeof(void*));
-    if (allocatedMemoryArray.data == NULL) {
-        exitWithError("Failed to resize memory array", MST_INTERNAL);
-    }
-}
-
-void addToDynamicArray(void* ptr) {
-    if (allocatedMemoryArray.size == allocatedMemoryArray.capacity) {
-        resizeDynamicArray();
-    }
-    allocatedMemoryArray.data[allocatedMemoryArray.size++] = ptr;
-}
-
-void removeFromDynamicArray(void* ptr) {
-    for (size_t i = 0; i < allocatedMemoryArray.size; i++) {
-        if (allocatedMemoryArray.data[i] == ptr) {
-            allocatedMemoryArray.data[i] = allocatedMemoryArray.data[--allocatedMemoryArray.size];
-            break;
-        }
-    }
-}
-
-void deallocateAllMemoryDynamicArray() {
-    for (size_t i = 0; i < allocatedMemoryArray.size; i++) {
-        free(allocatedMemoryArray.data[i]);
-    }
-    free(allocatedMemoryArray.data);
-    allocatedMemoryArray.size = 0;
-    allocatedMemoryArray.capacity = 0;
-}
 
 
 void *safe_MemoryBlock(size_t size) {
-    void* ptr = malloc(size);
-    if (ptr == NULL) {
-        exitWithError("Failed to allocate memory", MST_INTERNAL);
-    }
-    addToDynamicArray(ptr);
-    return ptr;
+    
 }
 
-void free_MemoryBlock(void* ptr) {
-    if (ptr != NULL) {
-        removeFromDynamicArray(ptr);
-        free(ptr);
-    }
+void free_MemoryBlock(void* current) {
+    
 }
 
 void free_all() {
-    deallocateAllMemoryDynamicArray();
+    
 }
 
-void exitWithError(char* msg, int errCode) {
-    fprintf(stderr, "Error: %s\n", msg);
+void exitWithError(char* masssage, int ErrCode) {
+    switch (ErrCode)
+    {
+    case ERR_NO:
+        fprintf(stderr,"There aren't any errors");
+        break;
+    case ERR_LEX:
+        fprintf(stderr,"Error in the program within the lexical analysis (incorrect structure of the current lexeme) line: %d",linenum);
+        break;
+    case ERR_SYNTAX:
+        fprintf(stderr,"Error in the parsing program line: %d",linenum);
+        break;
+    case ERR_SEMANT_FUNC_ARG:
+        fprintf(stderr,"Semantic error in the program - undefined function, variable redefinition line: %d",linenum);
+        break;
+    case ERR_SEMANT_PARAM:
+        fprintf(stderr,"Wrong number/type of parameters when calling the function or wrong type of return value from function line: %d",linenum);
+        break;
+    case ERR_SEMANT_UNDF_VALUE:
+        fprintf(stderr,"Semantic error in program - use of undefined or uninitialized for variable line: %d",linenum);
+        break;
+    case ERR_SEMANT_RETURN:
+        fprintf(stderr,"Missing/overrun expression in the return statement from the function line: %d",linenum);
+        break;
+    case ERR_SEMANT_TYPE:
+        fprintf(stderr,"Type compatibility semantic error in arithmetic, string, and relational expressions line: %d",linenum);
+        break;
+    case ERR_SEMANT_UNDF_TYPE:
+        fprintf(stderr,"The variable or parameter type is not specified and cannot be inferred from the expression used line: %d",linenum);
+        break;
+    case ERR_SEMANT_OTHER:
+        fprintf(stderr,"Other semantic errors: %d",linenum);
+        break;
+    case ERR_INTERNAL:
+        fprintf(stderr,"Internal compiler error, i.e. not affected by the input program line: %d",linenum);
+        break;
+    }
+    fprintf(stderr, "%s\n", masssage);
     free_all();
-    exit(errCode);
+    exit(ErrCode);
 }
 
-int printDebugMessage(const char* fmt, ...) {
+int printDebugMessage(const char* messsage, ...) {
     #ifdef DEBUG_MSG
     va_list args;
-    va_start(args, fmt);
-    int ret = vfprintf(stderr, fmt, args);
+    va_start(args, messsage);
+    int ret = vfprintf(stderr, messsage, args);
     va_end(args);
     return ret;
     #else
@@ -88,11 +76,11 @@ int printDebugMessage(const char* fmt, ...) {
     #endif
 }
 
-int printDebugMessage2(const char* fmt, ...) {
+int printDebugMessage2(const char* messsage, ...) {
     #ifdef DEBUG_MSG
     va_list args;
-    va_start(args, fmt);
-    int ret = vfprintf(stderr, fmt, args);
+    va_start(args, messsage);
+    int ret = vfprintf(stderr, messsage, args);
     va_end(args);
     return ret;
     #else
--- memory.h
+++ memory.h
@@ -3,40 +3,32 @@
 
 #include <stdio.h>
 
-#define MST_NO                 0
-#define MST_LEX                1 
-#define MST_SYNTAX             2 
-#define MST_SEMANT_FUNC_ARG    3
-#define MST_SEMANT_PARAM       4 
-#define MST_SEMANT_UNDF_VALUE  5
-#define MST_SEMANT_RETURN      6
-#define MST_SEMANT_TYPE        7
-#define MST_SEMANT_UNDF_TYPE   8
-#define MST_SEMANT_OTHER       9
-#define MST_INTERNAL           99
+#define ERR_NO                 0
+#define ERR_LEX                1 
+#define ERR_SYNTAX             2 
+#define ERR_SEMANT_FUNC_ARG    3
+#define ERR_SEMANT_PARAM       4 
+#define ERR_SEMANT_UNDF_VALUE  5
+#define ERR_SEMANT_RETURN      6
+#define ERR_SEMANT_TYPE        7
+#define ERR_SEMANT_UNDF_TYPE   8
+#define ERR_SEMANT_OTHER       9
+#define ERR_INTERNAL           99
+//#define DEBUG_MSG       1
 
-extern int srcLine;
+typedef struct MemoryList {
+    void* current;
+    struct MemoryList* next;
+} MemoryList;
+
+extern int linenum;
 extern int prgPass;
 
-typedef struct {
-    void** data;
-    int size;
-    int capacity;
-} DynamicArray;
-
-DynamicArray allocatedMemoryArray;
-
-
-void initializeDynamicArray();
-void resizeDynamicArray();
-void addToDynamicArray(void* ptr);
-void removeFromDynamicArray(void* ptr);
-void deallocateAllMemoryDynamicArray();
 void *safe_MemoryBlock(size_t size);
-void free_MemoryBlock(void* ptr);
+void free_MemoryBlock(void* current);
 void free_all();
-void exitWithError(char* msg, int errCode);
-int printDebugMessage(const char* fmt, ...);
-int printDebugMessage2(const char* fmt, ...);
+void exitWithError(char* messsage, int ErrCode);
+int printDebugMessage(const char* message, ...);
+int printDebugMessage2(const char* message, ...);
 
 #endif /* memory_h */
